0aa1e5dd98312cb0ec10689a715b37099db2babe
drathier <fille.haglund@hotmail.com>
2015-12-09 01:24:28 +0100

    Fixes formatting. Simplifies the CouchStore struct. Adds exponential backoff. Fixes some bugs where functions return nil instead of error on error. Adds function New for creating stores using an existing bucket.

--- couchbasestore.go
+++ couchbasestore.go
@@ -1,90 +1,88 @@
-//Copyright 2013 Srinath. All rights Reserved.
-//This Software is licensed under MIT license available in the LICENSE file.
-
+// Copyright 2013 Srinath. All rights Reserved.
+// Copyright 2015 drathier. All rights Reserved.
+// This Software is licensed under MIT license available in the LICENSE file.
+//
+// Couchbasestore is NOT thread-safe.
+//
+// This package is backwards compatible with the original package by Srinath.
+// The original can be found at https://github.com/srinathgs/couchbasestore
 package couchbasestore
 
 import (
 	"encoding/base32"
 	"errors"
-	"github.com/couchbaselabs/go-couchbase"
+	"github.com/couchbase/go-couchbase"
 	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
 	"net/http"
 	"strings"
+	"time"
 )
 
-//CouchStore Definition
+// CouchStore Definition
 type CouchStore struct {
-	endpoint   string
-	bucket     *couchbase.Bucket
-	codecs     []securecookie.Codec
-	options    *sessions.Options
-	pool       *couchbase.Pool
-	bucketName string
+	bucket  *couchbase.Bucket
+	codecs  []securecookie.Codec
+	options *sessions.Options
 }
 
-//No. Of Retries. The program that uses this package can change this.
+// Number of retries. The program that uses this package can change this.
 var Retries = 5
 
-//MaxAge Error.
+// Duration between retries. Increases exponentially with subsequent failed attempts.
+// Retries stop after roughly 1/2 * Retries*(Retries+1) * BaseRetryDuration.
+var BaseRetryDuration = 50 * time.Millisecond
+
+// MaxAge error.
 var ErrMaxAge = errors.New("max age should be greater than zero")
 
-//NewCouchStore creates a new CouchStore.
-func NewCouchStore(endpoint string, pool string, bucket string, path string, maxAge int, keyPairs ...[]byte) (*CouchStore, error) {
-	c, err := couchbase.Connect(endpoint)
-	if err != nil {
-		return nil, err
+// New creates a CouchStore using an existing couchbase.Bucket.
+func New(bucket *couchbase.Bucket, cookiePath string, cookieMaxAge int, keyPairs ...[]byte) (*CouchStore, error) {
+	if cookiePath == "" {
+		cookiePath = "/"
 	}
-	p, err := c.GetPool(pool)
-	if err != nil {
-		return nil, err
-	}
-	if path == "" {
-		path = "/"
-	}
-	if maxAge <= 0 {
+	if cookieMaxAge <= 0 {
 		return nil, ErrMaxAge
 	}
 	return &CouchStore{
-		endpoint: endpoint,
-		bucket:   nil,
-		codecs:   securecookie.CodecsFromPairs(keyPairs...),
+		bucket: bucket,
+		codecs: securecookie.CodecsFromPairs(keyPairs...),
 		options: &sessions.Options{
-			Path:   path,
-			MaxAge: maxAge,
+			Path:   cookiePath,
+			MaxAge: cookieMaxAge,
 		},
-		pool:       &p,
-		bucketName: bucket,
 	}, nil
 }
 
-//Internal Function to get a bucket from a pool
-func (c *CouchStore) getBucket() *couchbase.Bucket {
-	if c.bucket == nil {
-		c.bucket, _ = c.pool.GetBucket(c.bucketName)
-	}
-	return c.bucket
+// Backwards-compatible alias for New.
+func NewCouchStore(endpoint string, pool string, bucket string, cookiePath string, cookieMaxAge int, keyPairs ...[]byte) (*CouchStore, error) {
+	return NewFromURI(endpoint, pool, bucket, cookiePath, cookieMaxAge, keyPairs)
 }
 
-//Internal Function to close couchbase bucket
-func (c *CouchStore) closeBucket() {
+// NewFromURI creates a new CouchStore with a new couchbase.Bucket.
+func NewFromURI(endpoint string, pool string, bucket string, cookiePath string, cookieMaxAge int, keyPairs ...[]byte) (*CouchStore, error) {
+	bucket, err := couchbase.GetBucket(endpoint, pool, bucket)
+	if err != nil {
+		return err
+	}
+
+	return New(bucket, cookiePath, cookieMaxAge, keyPairs...)
+}
+
+// Close closes the bucket used by CouchStore.
+func (c *CouchStore) Close() {
 	c.bucket.Close()
 }
 
-//Close CouchStore
-func (c *CouchStore) Close() {
-	c.closeBucket()
-}
-
-//Get Session data from CouchStore. name is the key in the cookie against which the cookie string is set.
+// Get Session data from CouchStore. name is the key in the cookie against which the cookie string is set.
 func (c *CouchStore) Get(r *http.Request, name string) (*sessions.Session, error) {
 	return sessions.GetRegistry(r).Get(c, name)
 }
 
-//New creates a new session. CouchStore.Get will perform this in case there is no existing session.
+// New creates a new session. CouchStore.Get will perform this in case there is no existing session.
 func (c *CouchStore) New(r *http.Request, name string) (*sessions.Session, error) {
 	session := sessions.NewSession(c, name)
-	//default values
+	// default values
 	session.Options = &sessions.Options{
 		Path:   c.options.Path,
 		MaxAge: c.options.MaxAge,
@@ -103,48 +101,51 @@ func (c *CouchStore) New(r *http.Request, name string) (*sessions.Session, error
 	return session, err
 }
 
-//Save Session
+// Save saves the session to couchbase using bucket c.bucket.
 func (c *CouchStore) Save(r *http.Request, w http.ResponseWriter, session *sessions.Session) error {
 	// Build an alphanumeric key for the couchbase store.
 	if session.ID == "" {
 		session.ID = strings.TrimRight(base32.StdEncoding.EncodeToString(securecookie.GenerateRandomKey(32)), "=")
 	}
-	if err := c.save(session); err != nil {
-		return err
-	}
 	encoded, err := securecookie.EncodeMulti(session.Name(), session.ID, c.codecs...)
 	if err != nil {
+		return err
+	}
+
+	if err := c.save(session); err != nil {
 		return err
 	}
 	http.SetCookie(w, sessions.NewCookie(session.Name(), encoded, session.Options))
 	return nil
 }
 
-//Retry
-func mayBeRetry(f func() error) error {
-	var tries = 0
+// MaybeRetry runs the supplied function f until it no longer returns a timeout error, at most Retries times.
+// If it returns another error, it returns that error immediately.
+// Retries is a global variable.
+func maybeRetry(f func() error) error {
+	var failedAttempts = 0
 	var err error
-	for tries < Retries {
+	for failedAttempts < Retries {
 		err = f()
 		if err == couchbase.TimeoutError || err == couchbase.ErrTimeout {
-			tries++
-		} else if err == nil {
-			return nil
+			failedAttempts++
+			time.Sleep((failedAttempts + 1) * BaseRetryDuration) // exponential backoff
 		} else {
-			return err
+			return err // unknown or no error
 		}
 	}
 	return err
 }
 
-//Delete session from CouchStore and HTTP cookie
+// Delete deletes session from CouchStore and HTTP cookie stores.
 func (c *CouchStore) Delete(r *http.Request, w http.ResponseWriter, session *sessions.Session) error {
-	c.getBucket()
-	_ = mayBeRetry(func() error {
+	err := maybeRetry(func() error {
 		return c.bucket.Delete(session.ID)
 	})
+	if err != nil {
+		return err
+	}
 
-	//defer c.closeBucket()
 	// Set cookie to expire.
 	options := *session.Options
 	options.MaxAge = -1
@@ -156,33 +157,30 @@ func (c *CouchStore) Delete(r *http.Request, w http.ResponseWriter, session *ses
 	return nil
 }
 
-//Save session to Couchbase
+// Save saves the session to couchbase using bucket c.bucket.
 func (c *CouchStore) save(session *sessions.Session) error {
 	encoded, err := securecookie.EncodeMulti(session.Name(), session.Values, c.codecs...)
 	if err != nil {
 		return err
 	}
-	c.getBucket()
-	//defer c.closeBucket()
-	errSet := mayBeRetry(func() error {
+
+	err = maybeRetry(func() error {
 		return c.bucket.Set(session.ID, session.Options.MaxAge, encoded)
 	})
-	if errSet != nil {
-		return errSet
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
-//Load Session from couchbase
+// Load loads a session from couchbase using bucket c.bucket.
 func (c *CouchStore) load(session *sessions.Session) error {
-	c.getBucket()
-	//defer c.closeBucket()
 	var data interface{}
-	err := mayBeRetry(func() error {
+	err := maybeRetry(func() error {
 		return c.bucket.Get(session.ID, &data)
 	})
 	if err != nil {
-		return nil
+		return err
 	}
 	if data == nil {
 		return nil
